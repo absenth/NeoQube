f064296802be215a940e673307b1bcd0e5f412c8
absenth <1115138+absenth@users.noreply.github.com>
2025-07-10 21:50:54 -0500

    This connects and reads messages sent to twitch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+const CHANNELS: &[&str] = &["#absenth762"];
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut client = tmi::Client::anonymous().await?;
+    client.join_all(CHANNELS).await?;
+
+    loop {
+        let msg = client.recv().await?;
+        match msg.as_typed()? {
+            tmi::Message::Privmsg(msg) => {
+                println!("{}: {}", msg.sender().name(), msg.text());
+            }
+            tmi::Message::Reconnect => {
+                client.reconnect().await?;
+                client.join_all(CHANNELS).await?;
+            }
+            tmi::Message::Ping(ping) => {
+                client.pong(&ping).await?;
+            }
+            _ => {}
+        }
+    }
 }
