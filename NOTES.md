84a19f5daa6ba537b89e2a71cc014b48cff154ae
absenth <1115138+absenth@users.noreply.github.com>
2025-07-10 19:59:05 -0500

    this doesn't build

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,4 @@
 # Libraries
-- [twitch_irc](https://docs.rs/twitch-irc/latest/twitch_irc/)
-
+- [twitch_irc](https://lib.rs/crates/twitch-irc)
+- [dotenvy](https://lib.rs/crates/dotenvy)
+- [rust_webhook](https://lib.rs/crates/rust_webhook)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::TwitchIRCClient;
+use twitch_irc::{ClientConfig, SecureTCPTransport};
+
+#[tokio::main]
+pub async fn main() {
+    tracing_subscriber::fmt::init();
+
+    // default configuration is to join chat as anonymous.
+    // FIXME: configure this to use the botsenth545 user
+    let config = ClientConfig::default();
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    // first thing to do: start consuming incoming messages,
+    // otherwise they will back up.
+    let join_handle = tokio::spawn(async move {
+      while let Some(message) = incoming_messages.recv().await {
+            tracing::info!("Received message: {:?}", message);
+      }
+    });
+
+    // join a channel
+    // This function only returns an error if the passed channel login name is malformed,
+    // So in this simple case where the channel name is hardcoded we can ignore the potential
+    // error with `unwrap`.
+    client.join("absenth762".to_owned()).unwrap();
+
+    // keep the tokio executor alive.
+    // If you return instead of waiting the background task will exit.
+    join_handle.await.unwrap();
 }
+
